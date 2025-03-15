8c056e7f112b31e781cc42fd0be1a80c4c770950
MOUSSBEKK <146588891+MOUSSBEKK@users.noreply.github.com>
2025-03-15 17:32:35 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,6 +1,8 @@
 
 <?php
+
 session_start();
+
 
 
 $file = 'users.json';
@@ -55,34 +57,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body id="loginmenu">
 <header>
-    <nav>
-        <ul>
-            <li class="logo">
-                <img src="https://fontmeme.com/permalink/250208/ebb188615e03ca690752fd1065d0303e.png" alt="Logo" >
-            </li>
-            <li>
-                <a class="underline" href="../index.html">Home</a>
-            </li>
-            <li>
-                <a class="underline" href="book.html">Destinations</a>
-                <ul class="submenu">
-                    <li><a href="map.html">Map</a></li>
-                </ul>
+  <?php include("navbar.php")?>
 
-            </li>
 
-            <li>
-                <a class="underline" href="aboutus.html">About us</a>
-            </li>
-            <li class="research">
-                <a href="book.html"><i class='bx bx-search research'></i></a>
-            </li>
-            <li class="connect">
-                <a href="profil.php"><i class='bx bx-user-circle connect'></i></a>
-            </li>
-        </ul>
-    </nav>
 </header>
+
+
 
 <div class="wrapper">
     <form action="" method="POST">
