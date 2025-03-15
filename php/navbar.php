40e3b3e149d6285172c9473d01079ce57ebea8d4
MOUSSBEKK <146588891+MOUSSBEKK@users.noreply.github.com>
2025-03-15 17:31:58 +0100

    Create navbar.php

---
+++ php/navbar.php
@@ -0,0 +1,27 @@
+<nav>
+    <ul>
+        <li class="logo">
+            <img src="https://fontmeme.com/permalink/250208/ebb188615e03ca690752fd1065d0303e.png" alt="Logo" >
+        </li>
+        <li>
+            <a class="underline" href="../index.html">Home</a>
+        </li>
+        <li>
+            <a class="underline" href="book.html">Destinations</a>
+            <ul class="submenu">
+                <li><a href="map.html">Map</a></li>
+            </ul>
+
+        </li>
+
+        <li>
+            <a class="underline" href="aboutus.html">About us</a>
+        </li>
+        <li class="research">
+            <a href="book.html"><i class='bx bx-search research'></i></a>
+        </li>
+        <li class="connect">
+            <a href="profil.php"><i class='bx bx-user-circle connect'></i></a>
+        </li>
+    </ul>
+</nav>
