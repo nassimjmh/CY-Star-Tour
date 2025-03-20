8b02a1a086dce9054f5df0d127f88c4301c6f5b4
Nassim J <n455im07@gmail.com>
2025-03-20 11:22:13 +0100

    bug users.php

--- php/users.php
+++ php/users.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
-/*
+
 if ( !isset($_SESSION["role"]) || $_SESSION["role"] !== "Admin") {
-    
         header('location: ../../index.html');
-}else{
-    echo "Welcome Admin";
+        exit();
 }
-*/
 ?>
 
 <!DOCTYPE html>
