c81d9555a23c316c9a5a5fd3422ff8bf13913c11
Nassim J <n455im07@gmail.com>
2025-03-23 16:51:30 +0100

    icon

--- php/index.php
+++ php/index.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <title>StarTour - Home</title>
-        <link rel="icon" href="images/sparkles.png" type="image/png">
+        <link rel="icon" href="../images/sparkles.png" type="image/png">
         <link rel="stylesheet" href="../css/index.css?v=<?php echo time(); ?>">
         <link rel="stylesheet" href="../css/style.css?v=<?php echo time(); ?>">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
