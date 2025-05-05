3bc2de0efa02b077889e4021b423ac4f08eec9cf
MOUSSBEKK <146588891+MOUSSBEKK@users.noreply.github.com>
2025-05-05 12:15:20 +0200

    Update carouselle.php

--- php/carouselle.php
+++ php/carouselle.php
@@ -173,50 +173,10 @@
     <div class="dots" id="carouselDots"></div>
 </div>
 
-<script>
-    const track = document.getElementById('carouselTrack');
-    const slides = document.querySelectorAll('.item');
-    const dotsContainer = document.getElementById('carouselDots');
-    let currentSlide = 0;
 
-    slides.forEach((_, index) => {
-        const dot = document.createElement('span');
-        dot.classList.add('dot');
-        if (index === 0) dot.classList.add('active');
-        dot.addEventListener('click', () => goToSlide(index));
-        dotsContainer.appendChild(dot);
-    });
 
-    const dots = document.querySelectorAll('.dot');
+<script src="../js/carouselle.js"></script>
 
-    function updateCarousel() {
-        const slideWidth = slides[0].offsetWidth + 24; // ajusté selon le nouveau margin
-        const offset = (track.offsetWidth - slideWidth) / 2; // Pour centrer l'image principale
-        track.style.transform = `translateX(${-slideWidth * currentSlide + offset}px)`;
 
-        slides.forEach((slide, i) => {
-            slide.classList.toggle('active', i === currentSlide);
-            dots[i].classList.toggle('active', i === currentSlide);
-        });
-    }
-
-    function goToSlide(index) {
-        currentSlide = index;
-        updateCarousel();
-    }
-
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        updateCarousel();
-    }
-
-    let autoSlide = setInterval(nextSlide, 2500);
-
-    // Allow click on image to pause autoplay
-    track.addEventListener('mouseover', () => clearInterval(autoSlide));
-    track.addEventListener('mouseout', () => autoSlide = setInterval(nextSlide, 4000));
-
-    updateCarousel();
-</script>
 </body>
 </html>
